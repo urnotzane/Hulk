50486dea2dfabce3afac54585504175f1440cb04
urnotzane <urnotzane@163.com>
2020-04-05 02:59:05 +0800

    feat: 回文数删除代码注释

--- leetcode/9.回文数.ts
+++ leetcode/9.回文数.ts
@@ -11,14 +11,14 @@
  * - Your memory usage beats 35.25 % of javascript submissions (46.1 MB)
  * - 转换成字符串
  */
-// const isPalindrome = (x:number) => {
-//   const xStr = x.toString();
-//   const xStrReverse = xStr.split('').reverse().join('');
-//   if (+xStr === +xStrReverse) {
-//     return true;
-//   }
-//   return false;
-// };
+const isPalindrome = (x:number) => {
+  const xStr = x.toString();
+  const xStrReverse = xStr.split('').reverse().join('');
+  if (+xStr === +xStrReverse) {
+    return true;
+  }
+  return false;
+};
 
 /**
  * - Your runtime beats 14.56 % of javascript submissions
@@ -26,23 +26,23 @@
  * - 余数
  * - 求出反转后的数字进行比较，但是会多一半的循环
  */
-// const isPalindrome = (x:number) => {
-//   let left = x;
-//   let right = 0;
-//   while (left > 0) {
-//     const remainder = left % 10;
-//     left = Math.floor(left / 10);
-//     right = right * 10 + remainder;
-//   }
-//   return x === right;
-// }; 
+const isPalindrome2 = (x:number) => {
+  let left = x;
+  let right = 0;
+  while (left > 0) {
+    const remainder = left % 10;
+    left = Math.floor(left / 10);
+    right = right * 10 + remainder;
+  }
+  return x === right;
+}; 
 
 /**
  * - Your runtime beats 91.52 % of javascript submissions
  * - Your memory usage beats 95.8 % of javascript submissions (44.8 MB)
  * - 求余数，抽象为原始数字折叠比较左右
  */
-const isPalindrome = (x:number) => {
+const isPalindrome3 = (x:number) => {
   let left = x;
   let right = 0;
   // 两种特殊情况：原始数除以10没余数一定是false，x=0一定是true
